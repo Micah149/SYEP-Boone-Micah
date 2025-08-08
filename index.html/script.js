9af81dcaa7005a6f8a34782863fae4b38de93656
Micah149 <starsoul718@gmail.com>
2025-08-08 17:28:42 +0000

    Added in testimonial

--- index.html/script.js
+++ index.html/script.js
@@ -1,4 +1,74 @@
-console.log("Hello World");
+ const reviews =[
+    {
+        id: 1,
+        name:'Susan',
+        job: 'Teacher',
+        img: '#',
+        text: ' Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+ }, 
+{
+    id: 2,
+    name: 'Anna Johnson',
+    job: 'Teacher',
+    img: '#',
+    text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+},
+{
+    id: 3,
+    name: 'Peter Smith',
+    job: 'Teacher',
+    img: '#',
+    text: ' Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+}
+];
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentItem = 0;
+
+//Load Initial item
+window.addEventListener('DOMContentLoaded', function () {
+const item = reviews[currentItem];
+img.src = item.img;
+author.textContent = item.name;
+job.textContent = item.job;
+info.textContent = item.text;
+});
+
+function showPerson(person){
+    const item = reviews [person];
+     img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+ 
+ nextBtn.addEventListener('click', function (){
+      currentItem++;
+      if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+      }
+      showPerson(currentItem);
+ });
+
+ prevBtn.addEventListener('click' , function (){
+currentItem--;
+if(currentItem < 0){
+    currentItem = reviews.length - 1;
+}
+showPerson(currentItem);
+ });
+ 
+ 
+ 
+ 
+ /*console.log("Hello World");
 
 
 let myTest = 123;
